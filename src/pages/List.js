b6c35c9bce74dc716a137eb6124a2ca069d30d9c
Bernnuda <jeeee_in@naver.com>
2024-04-29 23:24:18 +0900

    기본파일셋팅

---
+++ src/pages/List.js
@@ -0,0 +1,10 @@
+import Nav from "../components/Nav";
+import "../styles/List.css";
+
+export default function listPage() {
+  return (
+    <>
+      <Nav />
+    </>
+  );
+}
---
+++ src/pages/Main.js
@@ -0,0 +1,10 @@
+import Nav from "../components/Nav";
+import "../styles/Main.css";
+
+export default function mainPage() {
+  return (
+    <>
+      <Nav />
+    </>
+  );
+}
