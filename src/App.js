641926a66cf7d6db4b0502c5fc147c807e00a929
gohansaem92 <gohansaem@gmail.com>
2024-04-29 13:55:30 +0900

    고한샘추가

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">김유미 테스트</div>;
+  return <div className="App">김유미 테스트고한샘추가</div>;
 }
 
 export default App;
