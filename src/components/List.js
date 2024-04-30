3c4e9fb430a01cb7e97b345dec37a17e93b0df81
Bernnuda <jeeee_in@naver.com>
2024-04-30 17:25:12 +0900

    feat: 페이지네이션 구현

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,8 @@
 import "../styles/List.css";
 import logo from "../assets/images/logo.svg";
 import arrow from "../assets/icons/Arrow-right-brown.svg";
+import arrow_left from "../assets/icons/Arrow-left.svg";
+import arrow_right from "../assets/icons/Arrow-right.svg";
 import message from "../assets/icons/Messages.svg";
 import { Link } from "react-router-dom";
 import Dropdown from "./Dropdown";
@@ -10,12 +12,15 @@ import { getListData } from "../api/api";
 export default function List() {
   const [order, setOrder] = useState("time");
   const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const res = await getListData();
         setData(res.results);
+        setTotalPages(Math.ceil(res.count / 8));
       } catch (e) {
         console.error(e);
       }
@@ -27,6 +32,38 @@ export default function List() {
   const handleSortOrderChange = (selectedOrder) => {
     setOrder(selectedOrder);
   };
+
+  const handlePageChange = (page) => {
+    if (
+      page < 1 ||
+      page > totalPages ||
+      (page === 1 && currentPage === 1) ||
+      (page === totalPages && currentPage === totalPages)
+    )
+      return;
+    setCurrentPage(page);
+  };
+
+  const renderPagination = () => {
+    const pageNumbers = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(
+        <div
+          key={i}
+          onClick={() => handlePageChange(i)}
+          className={"pageBtn " + (currentPage === i ? "active" : "")}
+        >
+          {i}
+        </div>
+      );
+    }
+    return pageNumbers;
+  };
+
+  const itemsPerPage = 8;
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
 
   return (
     <>
@@ -40,10 +77,10 @@ export default function List() {
         </Link>
       </div>
       <div className="list-main">
-        <h1 class="list-main-text">누구에게 질문할까요?</h1>
+        <h1 className="list-main-text">누구에게 질문할까요?</h1>
         <Dropdown onChange={handleSortOrderChange} />
         <div className="list-subjects">
-          {data.map((item) => (
+          {currentItems.map((item) => (
             <Link to={`/post/${item.id}`} key={item.id}>
               <div className="list-subject-container">
                 <div className="list-profile-container">
@@ -72,7 +109,25 @@ export default function List() {
           ))}
         </div>
       </div>
-      <div className="Pagnation"></div>
+      <div className="Pagnation">
+        <div className="pageBtn">
+          <img
+            src={arrow_left}
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+            alt="arrow_left"
+          />
+        </div>
+        {renderPagination()}
+        <div className="pageBtn">
+          <img
+            src={arrow_right}
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+            alt="arrow_right"
+          />
+        </div>
+      </div>
     </>
   );
 }
