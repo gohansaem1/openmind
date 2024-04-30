a04eede0de54260e58e240c2b23dd94ad0982cea
Bernnuda <jeeee_in@naver.com>
2024-04-30 17:01:48 +0900

    feat: 리스트 보여주기 구현 헤더제작, 정렬 디자인제작 페이지네이션 제작중

---
+++ src/components/Dropdown.js
@@ -0,0 +1,51 @@
+import { useState, useEffect, useRef } from "react";
+import arrow_down from "../assets/icons/Stroke-down.svg";
+import arrow_up from "../assets/icons/Arrow-up.svg";
+
+export default function Dropdown({ onChange }) {
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [order, setOrder] = useState("time");
+
+  const dropdownRef = useRef(null);
+  const toggleDropdown = () => {
+    setDropdownOpen((prev) => !prev);
+  };
+
+  const handleOrderChange = (order) => {
+    setOrder(order);
+    onChange(order);
+    setDropdownOpen(false);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+        setDropdownOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div className="dropdown-container">
+      <div className="dropdown" ref={dropdownRef}>
+        <div className="dropdown_button" onClick={toggleDropdown}>
+          <div className="select">
+            <span>{order}</span>
+            <img src={arrow_down} alt="arrow-down" />
+          </div>
+          {isDropdownOpen && (
+            <ul className="dropdown_contents">
+              <li onClick={() => handleOrderChange("name")}>이름순</li>
+              <li onClick={() => handleOrderChange("time")}>최신순</li>
+            </ul>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
