a6ec0002e369dc712878fb242148d1309bd3d826
song-ari <sari0708@ajou.ac.kr>
2024-05-01 23:41:26 +0900

    style: 질문 작성 Modal 컴포넌트 스타일 업데이트

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,3 +1,4 @@
+import "../styles/Modal.css";
 import messageIconBlack from "../assets/icons/Messages.svg";
 import closeIcon from "../assets/icons/Close.svg";
 import profile from "../assets/images/Photo.svg";
@@ -10,34 +11,45 @@ const Modal = ({ setIsModalOpen, modalBackgroundRef }) => {
   };
 
   return (
-    <div>
+    <>
       <div
-        className="modal-container"
+        className="modal-background"
         ref={modalBackgroundRef}
         onClick={onClickModal}
       >
-        <div className="modal-content">
-          <div>
-            <span className="modal-notice">
-              <img src={messageIconBlack} alt="messageIconBlack" /> 질문을
-              작성하세요
-            </span>
-            <img
-              src={closeIcon}
-              alt="closeIcon"
-              onClick={() => setIsModalOpen(false)}
-            />
+        <div className="modal-container">
+          <div className="modal-content">
+            <div className="modal-header">
+              <span className="modal-notice">
+                <img
+                  className="modal-icon"
+                  src={messageIconBlack}
+                  alt="messageIconBlack"
+                />{" "}
+                질문을 작성하세요
+              </span>
+              <img
+                className="modal-icon close"
+                src={closeIcon}
+                alt="closeIcon"
+                onClick={() => setIsModalOpen(false)}
+              />
+            </div>
+            <p className="modal-addressee">
+              To.
+              <img className="modal-icon" src={profile} alt="profile" />
+              마루는 고양이
+            </p>
+            <input
+              className="modal-input"
+              type="text"
+              placeholder="질문을 입력해주세요"
+            ></input>
+            <button className="modal-btn">질문 보내기</button>
           </div>
-          <p>
-            To.
-            <img src={profile} alt="profile" />
-            마루는 고양이
-          </p>
-          <input type="text"></input>
-          <button>질문 보내기</button>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
