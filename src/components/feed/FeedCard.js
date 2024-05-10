62607e710a13d7b78feefd7dd448dbf6b5da63f2
song-ari <sari0708@ajou.ac.kr>
2024-05-11 03:13:07 +0900

    refactor: Reactions 컴포넌트로 분리

--- src/components/feed/FeedCard.js
+++ src/components/feed/FeedCard.js
@@ -1,11 +1,7 @@
-import { useState } from "react";
 import timeString from "../../utils/timeString";
 import "../../styles/FeedCard.css";
 import AnswerBadge from "./AnswerBadge";
-import likeIconOff from "../../assets/icons/thumbs-up-gray.svg";
-import dislikeIconOff from "../../assets/icons/thumbs-down-gray.svg";
-import likeIconOn from "../../assets/icons/thumbs-up-blue.svg";
-import dislikeIconOn from "../../assets/icons/thumbs-down-blue.svg";
+import Reactions from "./Reactions";
 
 const FeedCard = ({ data, userData }) => {
     const { id, content, like, dislike, createdAt, answer } = data;
@@ -20,31 +16,6 @@ const FeedCard = ({ data, userData }) => {
     if (isRejected) {
         answerContent = "답변 거절";
     }
-
-    const [reaction, setReaction] = useState(null);
-    const [counts, setCounts] = useState({ like: like, dislike: dislike });
-
-    const handleReactionClick = (type) => {
-        if (reaction === type) {
-            setCounts((prevCounts) => ({
-                ...prevCounts,
-                [type]: prevCounts[type] - 1,
-            }));
-            setReaction(null);
-        } else {
-            setCounts((prevCounts) => ({
-                ...prevCounts,
-                [type]: prevCounts[type] + 1,
-            }));
-            if (reaction) {
-                setCounts((prevCounts) => ({
-                    ...prevCounts,
-                    [reaction]: prevCounts[reaction] - 1,
-                }));
-            }
-            setReaction(type);
-        }
-    };
 
     const formattedDate = timeString(createdAt);
     const answerFormattedDate = timeString(answerCreatedAt);
@@ -83,32 +54,7 @@ const FeedCard = ({ data, userData }) => {
                     </div>
                 )}
             </div>
-            <div className="FeedCard-reactionContainer">
-                <div
-                    className={`FeedCard-reaction ${reaction === "like" ? "clicked" : ""}`}
-                    onClick={() => handleReactionClick("like")}>
-                    <img
-                        src={reaction === "like" ? likeIconOn : likeIconOff}
-                        alt="likeIcon"
-                        className="FeedCard-reactionIcon"
-                    />
-                    좋아요 {counts.like > 0 && counts.like}
-                </div>
-                <div
-                    className={`FeedCard-reaction ${reaction === "dislike" ? "clicked" : ""}`}
-                    onClick={() => handleReactionClick("dislike")}>
-                    <img
-                        src={
-                            reaction === "dislike"
-                                ? dislikeIconOn
-                                : dislikeIconOff
-                        }
-                        alt="dislikeIcon"
-                        className="FeedCard-reactionIcon"
-                    />
-                    싫어요 {counts.dislike > 0 && counts.dislike}
-                </div>
-            </div>
+            <Reactions like={like} dislike={dislike} />
         </div>
     );
 };
