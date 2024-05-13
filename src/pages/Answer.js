518620edc93faafb6d835537d1b23496c5b9ef7f
김제완 <jeeee_in@naver.com>
2024-05-13 14:42:42 +0900

    refactor: 답변페이지 수정

--- src/pages/Answer.js
+++ src/pages/Answer.js
@@ -4,20 +4,24 @@ import { getQuestionList } from "../api/api";
 import { getUserData } from "../api/api";
 import { useState, useEffect } from "react";
 import { AnswerList } from "../components/feed/AnswerList";
+import { useParams } from "react-router-dom";
 
 const AnswerPage = () => {
     const [userData, setUserData] = useState({ data: null });
     const [questionList, setQuestionList] = useState({ data: null });
     const [rendering, setRendering] = useState(false);
 
+    const { id } = useParams();
+
     useEffect(() => {
         async function fetchData() {
-            const userData = await getUserData(5739);
-            const questionList = await getQuestionList(5739);
+            const userData = await getUserData(id);
+            const questionList = await getQuestionList(id);
             setUserData(userData);
             setQuestionList(questionList);
         }
         fetchData();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [rendering]);
 
     return (
