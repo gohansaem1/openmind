391bae6f0cff28b60514b170d9d234a0e3eb8971
김제완 <jeeee_in@naver.com>
2024-05-13 17:45:27 +0900

    refactor: 좋아요 싫어요 기능 수정

--- src/components/feed/Reactions.js
+++ src/components/feed/Reactions.js
@@ -1,43 +1,63 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 import "../../styles/Reactions.css";
 
 import likeIconOff from "../../assets/icons/thumbs-up-gray.svg";
 import dislikeIconOff from "../../assets/icons/thumbs-down-gray.svg";
 import likeIconOn from "../../assets/icons/thumbs-up-blue.svg";
 import dislikeIconOn from "../../assets/icons/thumbs-down-blue.svg";
+import { postReaction } from "../../api/api";
 
-const Reactions = ({ like, dislike }) => {
+const Reactions = ({ like, dislike, id }) => {
     const [reaction, setReaction] = useState(null);
     const [counts, setCounts] = useState({ like: like, dislike: dislike });
 
+    useEffect(() => {
+        const storedReaction = localStorage.getItem(`${id}_reaction`);
+        if (storedReaction) {
+            setReaction(storedReaction);
+        }
+    }, [id]);
+
     const handleReactionClick = (type) => {
-        if (reaction === type) {
-            setCounts((prevCounts) => ({
-                ...prevCounts,
-                [type]: prevCounts[type] - 1,
-            }));
-            setReaction(null);
-        } else {
-            setCounts((prevCounts) => ({
-                ...prevCounts,
-                [type]: prevCounts[type] + 1,
-            }));
-            if (reaction) {
-                setCounts((prevCounts) => ({
-                    ...prevCounts,
-                    [reaction]: prevCounts[reaction] - 1,
-                }));
-            }
+        // if (reaction === type) {
+        //     localStorage.removeItem(`${id}_reaction`);
+        //     setCounts((prevCounts) => ({
+        //         ...prevCounts,
+        //         [type]: prevCounts[type] - 1,
+        //     }));
+        //     setReaction(null);
+        // } else {
+        //     localStorage.setItem(`${id}_reaction`, type);
+        //     setCounts((prevCounts) => ({
+        //         ...prevCounts,
+        //         [type]: prevCounts[type] + 1,
+        //     }));
+        //     if (reaction) {
+        //         setCounts((prevCounts) => ({
+        //             ...prevCounts,
+        //             [reaction]: prevCounts[reaction] - 1,
+        //         }));
+        //     }
+        //     setReaction(type);
+        // }
+
+        if (reaction === null) {
+            postReaction(id, type);
+            localStorage.setItem(`${id}_reaction`, type);
             setReaction(type);
+            setCounts((prev) => ({
+                ...prev,
+                [type]: prev[type] + 1,
+            }));                        
         }
     };
 
     return (
         <div className="FeedCard-reactionContainer">
             <div
-                className={`FeedCard-reaction ${reaction === "like" ? "clicked" : ""}`}
-                onClick={() => handleReactionClick("like")}>
+                className={`FeedCard-reaction ${reaction === "like" ? "clicked" : reaction === "dislike" ? "another" : ""}`}
+                onClick={() => handleReactionClick("like")}
+            >
                 <img
                     src={reaction === "like" ? likeIconOn : likeIconOff}
                     alt="likeIcon"
@@ -46,12 +66,11 @@ const Reactions = ({ like, dislike }) => {
                 좋아요 {counts.like > 0 && counts.like}
             </div>
             <div
-                className={`FeedCard-reaction ${reaction === "dislike" ? "clicked" : ""}`}
-                onClick={() => handleReactionClick("dislike")}>
+                className={`FeedCard-reaction ${reaction === "dislike" ? "clicked" : reaction === "like" ? "another" : ""}`}
+                onClick={() => handleReactionClick("dislike")}
+            >
                 <img
-                    src={
-                        reaction === "dislike" ? dislikeIconOn : dislikeIconOff
-                    }
+                    src={reaction === "dislike" ? dislikeIconOn : dislikeIconOff}
                     alt="dislikeIcon"
                     className="FeedCard-reactionIcon"
                 />
