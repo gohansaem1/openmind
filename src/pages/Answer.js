4aeaf3e2ae256f84104014363c3a1a8169dd5ded
gohansaem92 <gohansaem@gmail.com>
2024-05-14 19:55:20 +0900

    fix: 답변페이지 더보기추가

--- src/pages/Answer.js
+++ src/pages/Answer.js
@@ -7,34 +7,55 @@ import { AnswerList } from "../components/feed/AnswerList";
 import { useParams } from "react-router-dom";
 
 const AnswerPage = () => {
-    const [userData, setUserData] = useState({ data: null });
-    const [questionList, setQuestionList] = useState({ data: null });
+    const [userData, setUserData] = useState({});
+    const [questionList, setQuestionList] = useState([]);
     const [rendering, setRendering] = useState(false);
+    const [nextPage, setNextPage] = useState("");
+    const [loading, setLoading] = useState(false);
 
     const { id } = useParams();
 
     useEffect(() => {
         async function fetchData() {
             const userData = await getUserData(id);
-            const questionList = await getQuestionList(id);
+            const data = await getQuestionList(id);
             setUserData(userData);
-            setQuestionList(questionList);
+            setQuestionList(data.results);
+            setNextPage(data.next);
         }
         fetchData();
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [rendering]);
+    }, [rendering, questionList.count]);
 
+    const handleLoadMore = async () => {
+        setLoading(true);
+        try {
+            const data = await getQuestionList(id, nextPage);
+
+            setQuestionList([...questionList, ...data.results]);
+            setNextPage(data.next);
+        } catch (e) {
+            console.error("Error fetching more data", e);
+        } finally {
+            setLoading(false);
+        }
+    };
     return (
         <>
             <Header userData={userData} />;
             <div className="Post-background">
-                <AnswerList
-                    subjectId={id}
-                    userData={userData}
-                    questionList={questionList}
-                    rendering={rendering}
-                    setRendering={setRendering}
-                />
+                <div className="answer-container">
+                    <AnswerList
+                        subjectId={id}
+                        userData={userData}
+                        questionList={questionList}
+                        rendering={rendering}
+                        setRendering={setRendering}
+                        loading={loading}
+                        nextPage={nextPage}
+                        handleLoadMore={handleLoadMore}
+                    />
+                </div>
             </div>
         </>
     );
